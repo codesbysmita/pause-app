1986eaa620ac977070cc86d9d06f6bc1aa80eee7
Smita Jena <smitajena754@gmail.com>
2026-01-03 10:28:13 +0530

    Update script.js (day3)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-/* ================================
-   Today.
-   Day 1 — Project Foundation
-================================ */
+//  Elements 
+const startBtn = document.querySelector('.primary-btn');
+const panel = document.querySelector('.write-panel');
+const closeBtn = document.querySelector('.close-btn');
 
-// JavaScript logic will be added step by step
+//  Open Panel 
+startBtn.addEventListener('click', () => {
+  panel.classList.add('show');
+  panel.classList.remove('hidden');
+});
+
+// Close Panel 
+closeBtn.addEventListener('click', () => {
+  panel.classList.remove('show');
+
+  // wait for animation before disabling clicks
+  setTimeout(() => {
+    panel.classList.add('hidden');
+  }, 400);
+});
